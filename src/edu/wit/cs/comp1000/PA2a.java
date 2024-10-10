f273f8c541a3344170b32648845e9ee912ae45be
Nguye <Nguye@10.220.88.116>
2024-10-10 12:37:31 -0400

    Programming Assignment 2 Code

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,45 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA2a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int sumPostive = 0, sumNonPostive = 0, sumAll = 0;
+		int countPostive = 0, countNonPostive = 0;
+		
+		System.out.printf("Enter five whole numbers: ");
+		for (int i = 0; i < 5; i++) {
+			int num = scanner.nextInt();
+			sumAll = sumAll + num;
+			
+			if (num > 0) {
+				sumPostive = sumPostive + num;
+				countPostive = countPostive + 1;
+			} else {
+				sumNonPostive = sumNonPostive + num;
+				countNonPostive = countNonPostive + 1;
+			}
+			
+		}
+		
+		double averagePostive = countPostive > 0 ? (double) sumPostive / countPostive: 0; //asked tutor, taught me about (double), still kinda confused about it even after doing more research but I couldn't find another way for it to work
+		double averageNonPostive = countNonPostive > 0 ? (double) sumNonPostive / countNonPostive: 0;
+		double averageAll = (double) sumAll / 5;
+		
+		System.out.printf("The sum of the %d positive numbers: %d%n", countPostive, sumPostive);
+		System.out.printf("The sum of the %d non-postive numbers: %d%n", countNonPostive, sumNonPostive);
+		System.out.printf("The sum of the 5 numbers: %d%n ", sumAll);
+		
+		System.out.printf("The average of the %d postive numbers: %.2f%n", countPostive, averagePostive);
+		System.out.printf("The average of the %d non-postive numbers: %.2f%n", countNonPostive, averageNonPostive);
+		System.out.printf("The average of the 5 numbers: %.2f%n", averageAll);
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,38 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA2b {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.printf("Enter a, b, c: ");
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
+		double c = scanner.nextDouble();
+		
+		double discriminant = b * b - 4 * a * c;
+		
+		if (discriminant < 0) {
+			System.out.printf("Roots: imaginary");
+		} else if (discriminant == 0) {
+			double root = -b / (2 * a);
+			System.out.printf("Root: %.2f%n", root);
+		} else {
+			double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+			double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+			
+			if (root1 > root2) {
+				System.out.printf("Roots: %.2f, %.2f%n", root2, root1);
+			} else {
+				System.out.printf("Roots: %.2f, %.2f%n", root1, root2);
+			}
+		}
 	}
 
 }
